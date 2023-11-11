80df34c08c037f20e5b7a023ebcc1c634166af70
Talal Syed <3298457+stalal@users.noreply.github.com>
2023-11-11 13:50:17 -0500

    finished the basic functionality for the blog

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
 
   root "articles#index"
 
-  get "/articles", to: "articles#index"
+  resources :articles
+  
+  # get "/articles", to: "articles#index"
+
+  # get "/articles/:id", to: "articles#show"
   
   get "up" => "rails/health#show", as: :rails_health_check
 
