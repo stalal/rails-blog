f95588151eec04c631c1e53a11121bf89738ece4
Talal Syed <3298457+stalal@users.noreply.github.com>
2023-11-11 22:51:04 -0500

    added comments to each blog post

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,9 @@ Rails.application.routes.draw do
 
   root "articles#index"
 
-  resources :articles
-  
-  # get "/articles", to: "articles#index"
-
-  # get "/articles/:id", to: "articles#show"
+  resources :articles do 
+    resources :comments
+  end
   
   get "up" => "rails/health#show", as: :rails_health_check
 
